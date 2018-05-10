ed0d3544477009624d1fff49b70c4e6e611f8b4c
Abhinav <katiyar.abhinav@gmail.com>
2018-05-10 15:06:19 -0700

    Updated image

--- sense/ros_entrypoint.sh
+++ sense/ros_entrypoint.sh
@@ -2,5 +2,8 @@
 set -e
 
 # setup ros environment
+echo "source /opt/ros/$ROS_DISTRO/setup.bash">> ~/.bashrc
+echo "source /root/store/devel/setup.bash">> ~/.bashrc
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+source "/root/store/devel/setup.bash"
 exec "$@"
